247e5d1bb9be8b06926d9cc07f09d9304d273986
lorcx <740707954@qq.com>
2017-10-14 15:56:00 +0800

    2017年10月13日 11:58:58

--- src/main/java/review/BinarySearch.java
+++ src/main/java/review/BinarySearch.java
@@ -32,9 +32,36 @@ public class BinarySearch {
         return -1;
     }
 
+    /**
+     * 递归二分查找法
+     * @param list 要查找的数组
+     * @param key  要查找的值
+     * @param comp 排序规则
+     * @param <T>
+     * @return
+     */
+    public static <T> int recursionSearch(T[] list, T key, Comparator comp) {
+       return recursionSearch(list, key, comp, 0, list.length - 1);
+    }
+
+    public static <T> int recursionSearch(T[] list, T key, Comparator comp, int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) >>> 1;
+            int n = comp.compare(list[mid], key);
+            if (n > 0) {
+                high = mid - 1;
+            } else if (n < 0) {
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         Integer[] inArr = new Integer[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int n = search(inArr, 10, (o1, o2) -> ((int) o1) - ((int) o2));
+        int n = recursionSearch(inArr, 10, (o1, o2) -> ((int) o1) - ((int) o2));
         System.out.println(n);
     }
 }
