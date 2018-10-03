378a4a8d2852dc0f95a3b4d4491f432c677a4552
lorcx <740707954@qq.com>
2018-10-03 15:37:10 +0800

    commit

--- src/main/java/design_model/proxy/shangxuetang/moniJDK/Test1.java
+++ src/main/java/design_model/proxy/shangxuetang/moniJDK/Test1.java
@@ -1,18 +1,20 @@
 package design_model.proxy.shangxuetang.moniJDK;
 
-import java.lang.reflect.Constructor;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import org.omg.CORBA.portable.InvokeHandler;
-import design_model.proxy.shangxuetang.MoveAble;
-import design_model.proxy.shangxuetang.TankLogProxy;
+
+import java.lang.reflect.InvocationTargetException;
+import java.math.BigDecimal;
 
 public class Test1 {
 	 String name;
 	 InvokeHandler handler;
 	 
 	public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		Method[] methods = TankLogProxy.class.getMethods();
+		BigDecimal b2 = new BigDecimal(0.1);
+		BigDecimal b1 = BigDecimal.valueOf(0.1);
+		System.out.println(b1);//0.1
+		System.out.println(b2);//0.1000000000000000055511151231257827021181583404541015625
+		/*Method[] methods = TankLogProxy.class.getMethods();
 		for (Method m : methods) {
 			System.out.println(m);
 		}
@@ -25,6 +27,6 @@ public class Test1 {
 		
 		InvokeHandler h = null ;
 		Constructor ctr = c.getConstructor(InvokeHandler.class);
-		Object able = ctr.newInstance(h.getClass());	
+		Object able = ctr.newInstance(h.getClass());	*/
 	}
 }
